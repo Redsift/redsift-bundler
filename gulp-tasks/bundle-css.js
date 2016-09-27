ff2e88aa3e7b1c2d7bf9bcb3a7606b29ec640d43
Philippe de Reynal <philippe@dereynal.com>
2016-09-27 12:24:10 +0100

    add stylus imports from node_modules/

--- gulp-tasks/bundle-css.js
+++ gulp-tasks/bundle-css.js
@@ -109,6 +109,7 @@ function bundleStyles(gulp, opts) {
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(stylus({
+      'include': path.join(process.cwd(), "./node_modules/"),
       'include css': true
     }))
     .pipe(concat(opts.name + '.css'))
