ee160bc2922857578e3e0a3503ee1e159c17373a
Martin Hecher <martin@redsift.io>
2016-12-05 12:06:52 +0000

    replace NODE_ENV with 'DefinePlugin'

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -62,6 +62,9 @@ module.exports = {
     // }],
   },
   plugins: [
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
